9670c515ed0e975438a2cdd670357634486b7ef2
Joel Klemens <joel.klemens@gmail.com>
2022-01-25 12:16:14 -0800

    Created app and conntected to amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "rnamplifytodo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
